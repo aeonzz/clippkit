629d33bf2267975f0694dfcbd304899022053820
Sam <sambowenhughes@hotmail.co.uk>
2025-05-10 07:36:31 +0200

    Added some new registry bits

---
+++ apps/docs/public/r/default/lib/utils.ts
@@ -0,0 +1,11 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function getIsExternalLink(href: string) {
+  return href.startsWith("http") || href.startsWith("https");
+}
+
