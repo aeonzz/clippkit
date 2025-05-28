e0f304894646415a2d7a5368d7ad220616561ca1
Sam <sambowenhughes@hotmail.co.uk>
2025-05-28 10:03:59 +0100

    Updated bits on clippkit

--- apps/docs/config/site.ts
+++ apps/docs/config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Clippkit",
+  name: "",
   description:
     "A collection of beautifully-crafted video components. Text effects, intros, transitions, and more. Built for your next video project.",
   url: "https://clippkit.com",
